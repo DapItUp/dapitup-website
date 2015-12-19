e998112e9ba0126876fff55d51533e05e0991be0
Mike Huebner <mikehuebner@me.com>
2015-12-19 12:42:41 -0800

    - Working on how the box actually works, I'll leave the rest to you :D

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,56 +1,94 @@
-//These hide/show the men/women sections
+/**
+ * @name App
+ * @description Toggle the mens and womans clothes along with the items within each container.
+ * @todo Figure out a way to toggle each item but don't lose their current state if they have been touched or moved.
+ */
+
+ $('.toggle-items').click(toggleItems);
+ adjustShopBoxHeight();
 
 $('#men').change(function() {
-	$('.men').toggle();
+    $('.men').toggle();
+	adjustShopBoxHeight();
 });
 
 $('#women').change(function() {
-	$('.women').toggle();
+    $('.women').toggle();
+	adjustShopBoxHeight();
 });
 
-//This code hides/shows the item area 
-$('.toggle-items').click(function() {
-	$('.shop-items').toggle('slow');
-	$('.men-women-checkbox').toggle();
-	if($(this).hasClass('transform')) {
-		$(this).removeClass('transform');
-		$('.mannequin').removeClass('top');
-	} else {
-		$(this).addClass('transform');
-		$('.mannequin').addClass('top');
-	}
-});
 
 //This code cycles through the shopping categories
 $('.to-shirts').click(function() {
-	$('.hats').hide();
-	$('.shirts').show();
+	console.log("testing");
+    $('.hats').css('visibility', 'hidden');
+    $('.shirts').css('visibility', 'visible');
+	adjustShopBoxHeight();
 });
 
 $('.to-bottoms').click(function() {
-	$('.shirts').hide();
-	$('.bottoms').show();
+    $('.shirts').css('visibility', 'hidden');
+    $('.bottoms').css('visibility', 'visible');
+	adjustShopBoxHeight();
 });
 
 $('.to-hats').click(function() {
-	$('.bottoms').hide();
-	$('.hats').show();
+    $('.bottoms').css('visibility', 'hidden');
+    $('.hats').css('visibility', 'visible');
+	adjustShopBoxHeight();
 });
-
-
-//This code is for the drag/drop system of the app
 
 // target elements with the "draggable" class
 interact('.shop-item img')
-  .draggable({
-    // enable autoScroll
-    autoScroll: true,
+    .draggable({
+        // enable autoScroll
+        autoScroll: true,
 
-    // call this function on every dragmove event
-    onmove: dragMoveListener,
-  });
+        // call this function on every dragmove event
+        onmove: dragMoveListener,
+    });
 
-  function dragMoveListener (event) {
+// When we resize, we need to keep that current state visible on the screen.
+window.dragMoveListener = dragMoveListener;
+
+//
+// Functions
+//
+
+/**
+ * @function toggleItems
+ * @description Toggle the items in the box
+ */
+function toggleItems() {
+    $('.shop-items').toggle('slow');
+    $('.men-women-checkbox').toggle();
+
+    if ($(this).hasClass('transform')) {
+        $(this).removeClass('transform');
+        $('.mannequin').removeClass('top');
+    } else {
+        $(this).addClass('transform');
+        $('.mannequin').addClass('top');
+    }
+}
+
+/**
+ * @function adjustShopBoxHeight
+ * @description Fix the height of the box when an event happens that requires a resize.
+ */
+function adjustShopBoxHeight() {
+	$('.shop-items').css({
+		'height': $('.shop-category').height() + 50,
+		'min-height': 400
+	});
+}
+
+/**
+ * @function dragMoveListener
+ * @description Move the objects within the container
+ * @param  {Object} event 	Event of the movement
+ */
+function dragMoveListener(event) {
     var target = event.target,
         // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
@@ -58,14 +96,11 @@ interact('.shop-item img')
 
     // translate the element
     target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
 
     // update the posiion attributes
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
-  }
-
-  // this is used later in the resizing and gesture demos
-  window.dragMoveListener = dragMoveListener;
-
+	$(target).addClass('touched');
+}
